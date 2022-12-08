cfba44e92f21373f2505e44e4b3805d8aa9d9804
Oyindamola Ogunknle <oyinwumi@gmail.com>
2022-12-09 00:22:29 +0100

    chngs

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -5,22 +5,24 @@ import axios from 'axios';
 
 
 const UserCard = () => {
-    const [users, setUsers] = useState([])
-    const [searchTerm, setSearchTerm] = useState("")
+    const [users, setUsers] = useState([]);
+    // const [searchTerm, setSearchTerm] = useState("");
+
 
     useEffect(()=>{
       const getApi = async ()=>{
         const response = await axios.get("https://jsonplaceholder.typicode.com/users");
         setUsers(response.data)
-        setSearchTerm('users')
+       
       }
       getApi();
     }, [])
+   
   return (
     <div className='body'>
         <div>
-        <input type="text" placeholder='search for user' onChange={(e) => setSearchTerm(e.target.value)} />
-        <i class="fa-solid fa-magnifying-glass"  onClick={()=>setSearchTerm(searchTerm)}></i>
+        {/* <input type="text" placeholder='search for user' onChange={(e) => setSearchTerm(e.target.value)} />
+        <i class="fa-solid fa-magnifying-glass"  onClick={()=>setSearchTerm(searchTerm)}></i> */}
        
         </div>
       <div className='card-container'>
